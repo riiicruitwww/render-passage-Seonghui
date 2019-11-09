13bb2658b0264ef5c88167baac6d5212ceb899a6
Seonghui <stellashyou@gmail.com>
2019-11-10 01:07:07 +0900

    API 서버 생성

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
-const task = require('../task_container.json');
+var cors = require('cors');
+const task = require('./data/task_container.json');
+
+app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/api/task', (req, res) => res.json(task));
 
